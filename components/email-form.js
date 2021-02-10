26c6cce1b3d8b67239c9e0ae1fb3778a9bd20f11
Phil Leggetter <phil@leggetter.co.uk>
2021-02-10 01:15:27 +0000

    fix: ensure email input has a name for POST

--- components/email-form.js
+++ components/email-form.js
@@ -3,7 +3,7 @@ import styles from '../styles/EmailForm.module.css'
 export default function EmailForm({ buttonText = "Submit", actionUrl }) {
     return (
         <form method="post" action={actionUrl}>
-            <input type="email" required placeholder="email" className={styles.email} />
+            <input type="email" name="email" required placeholder="email" className={styles.email} />
             <button>{buttonText}</button>
         </form>
     )
