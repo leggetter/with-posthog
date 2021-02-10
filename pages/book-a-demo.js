fb6a19799df94370160007e371c71b08772c0ec6
Phil Leggetter <phil@leggetter.co.uk>
2021-02-10 00:57:58 +0000

    feat: book a demo flow

--- pages/book-a-demo.js
+++ pages/book-a-demo.js
@@ -1,9 +1,13 @@
 import Layout from '../components/layout'
+import PageHero from '../components/page-hero'
+import EmailForm from '../components/email-form'
 
 export default function BookADemo() {
   return (
-    <Layout>
-        <div>Book a Demo</div>
+    <Layout title="Sign Up">
+        <PageHero title="🎟 Book a Demo" description="Here's what you'll learn in the demo 🎓" />
+
+        <EmailForm buttonText="Book a Demo" actionUrl="/api/book-demo" />
     </Layout>
-    )
+  )
 }
---
+++ pages/confirm-demo.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout'
+
+import PageHero from '../components/page-hero'
+
+export default function ConfirmDemo() {
+  return (
+    <Layout title="Thanks!">
+        <PageHero title="👍 Thanks!" description="Thanks for registering for demo. We'll be in touch 📩" />
+    </Layout>
+  )
+}
